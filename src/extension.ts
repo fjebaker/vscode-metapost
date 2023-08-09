8895eaec0a1fba3555cea8735f1b6623bb3c573a
fjebaker <fergusbkr@gmail.com>
2023-08-09 15:29:49 +0100

    fix: preview working again + cleaner log output

--- src/extension.ts
+++ src/extension.ts
@@ -5,23 +5,23 @@ import * as viewer from './viewer';
 export const logChannel = vscode.window.createOutputChannel('Metapost');
 
 export function activate(context: vscode.ExtensionContext) {
-
-
 	context.subscriptions.push(
 		logChannel,
 		vscode.commands.registerCommand('metapost.run', async () => {
-			logChannel.appendLine("> Run");
+			logChannel.appendLine("INFO: called Run");
 			await compiler.compileFile();
 		}),
 		vscode.commands.registerCommand('metapost.cleanDir', () => {
-			logChannel.appendLine("> CleanDir");
+			logChannel.appendLine("INFO: called CleanDir");
 			compiler.cleanDirectory();
 		}),
 		vscode.commands.registerCommand('metapost.previewSide', async () => {
-			logChannel.appendLine("> PreviewSide");
+			logChannel.appendLine("INFO: called PreviewSide");
 			let previewer : viewer.Previewer | undefined = viewer.newPreviewer(context);
 			if (previewer) {
 				await viewer.updatePreview(previewer);
+			} else {
+				logChannel.appendLine("WARN: no previewer created.");
 			}
 		})
 	);
--- src/viewer.ts
+++ src/viewer.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
-import * as fs from 'fs';
 
 import * as compiler from './compiler';
 import { getWebviewContent, updateErrorLog, updateImageUri } from './webview';
@@ -28,7 +27,7 @@ interface Previewer {
  */
 function configureHooks(p: Previewer, context: vscode.ExtensionContext) : vscode.Disposable {
     const listener : vscode.Disposable = vscode.workspace.onDidSaveTextDocument(async (doc) => {
-        logChannel.appendLine("Save detected; reloading preview.");
+        logChannel.appendLine("INFO: save hook called: reloading preview");
         const stdErr = await updatePreview(p);
     });
     
@@ -41,105 +40,91 @@ function configureHooks(p: Previewer, context: vscode.ExtensionContext) : vscode
 }
 
 async function getPreviewChoice(files: string[]) : Promise<string | undefined> {
-    let images = files.filter( i => path.parse(i).ext != ".log");
-    const result = await vscode.window.showQuickPick(
+    let images = files.filter( i => path.parse(i).ext !== ".log");
+    logChannel.appendLine(`INFO: asking for selection for "${images}"`);
+    return vscode.window.showQuickPick(
         images,
         {
             placeHolder: "Select a figure to use in the preview.",
         }
     );
-    return result;
 }
 
 async function updatePreview(p: Previewer) : Promise<string | undefined> {
-
-    // do the build
-    if (p.panel) {
-
-        logChannel.appendLine("Updating preview:");
-
-        const buildOut : compiler.BuildOutput = await compiler.buildFigureFile(p.workDir, p.srcFile);
-        
-        if (! buildOut.err) {
-            logChannel.appendLine(` - messaging new image to webview.`);
-
-            if (! p.previewFile) {
-
-                const fileChoice = await getPreviewChoice(buildOut.outputFiles);
-
-                if (! fileChoice) {
-
-                    updateErrorLog(p.panel, "Bad file choice for preview. Close this tab and try again.");
-                    return undefined;
-
-                } else {
-                    p.previewFile = fileChoice;
-                }
-            }
-
-            updateImageUri(p.panel, path.join(p.workDir, p.previewFile));
-
-        } else {
-            logChannel.appendLine(` - messaging error to webview.`);
-            updateErrorLog(p.panel, buildOut.stdout);
-        }
-
-    } else {
-
-        logChannel.appendLine("Error making temporary figure file / bad panel!");
-
+    if (!p.panel) {
+        logChannel.appendLine("ERROR: making temporary figure file / bad panel");
         return undefined;
     }
+
+    logChannel.appendLine(`INFO: updating preview for "${p.srcFile}"`);
+    const buildOut : compiler.BuildOutput = await compiler.buildFigureFile(p.workDir, p.srcFile);
+
+    if (buildOut.err) {
+        logChannel.appendLine(`ERROR: build error: log in webview`);
+        updateErrorLog(p.panel, buildOut.stdout);
+    }
+    
+    logChannel.appendLine(`INFO: build success: updating webview`);
+
+    if (! p.previewFile) {
+        const fileChoice = await getPreviewChoice(buildOut.outputFiles);
+        logChannel.appendLine(`INFO: selected "${fileChoice}"`);
+
+        if (! fileChoice) {
+            updateErrorLog(p.panel, "Bad file choice for preview. Close this tab and try again.");
+            return undefined;
+        } else {
+            p.previewFile = fileChoice;
+        }
+    }
+    updateImageUri(p.panel, path.join(p.workDir, p.previewFile));
 }
 
 function newPreviewer(context : vscode.ExtensionContext ) : Previewer | undefined {
-
     const srcDoc = vscode.window.activeTextEditor?.document; 
     const cwd = srcDoc ? path.parse(srcDoc.fileName).dir : undefined;
 
-    if (cwd) {
-
-        logChannel.appendLine(`Working directory: ${cwd.toString()}.`);
-
-        // create panel
-        const panel = vscode.window.createWebviewPanel(
-            'metapostPreview',
-            'Metapost Preview',
-            vscode.ViewColumn.Two,
-            {
-                enableScripts: true
-            }
-        );
-
-
-        panel.webview.html = getWebviewContent();
-
-        let srcFileName = srcDoc ? path.parse(srcDoc.fileName).base : "";
-
-        let previewer : Previewer = {
-            panel: panel, 
-            srcFile: srcFileName,
-            workDir: cwd,
-            previewFile: undefined
-        };
-
-        let onSave = configureHooks(previewer, context);
-
-        // disposer
-        panel.onDidDispose(() => {
-            logChannel.appendLine("PANEL DISPOSED");
-            onSave.dispose();
-        })
-
-        logChannel.appendLine("Preview panel configured.");
-
-        return previewer;
-
-    } else {
-
-        logChannel.appendLine("No working directory assigned.");
+    if (!cwd) {
+        logChannel.appendLine("WARN: no working directory assigned");
         return undefined;
     }
+
+    logChannel.appendLine(`INFO: working directory: ${cwd.toString()}`);
+
+    // create panel
+    const panel = vscode.window.createWebviewPanel(
+        'metapostPreview',
+        'Metapost Preview',
+        // enable to the right of current panel
+        vscode.ViewColumn.Two,
+        {
+            enableScripts: true
+        }
+    );
+
+    panel.webview.html = getWebviewContent();
+
+    let srcFileName = srcDoc ? path.parse(srcDoc.fileName).base : "";
+    logChannel.append(`DEBUG: srcFileName: "${srcFileName}"`);
+
+    let previewer : Previewer = {
+        panel: panel, 
+        srcFile: srcFileName,
+        workDir: cwd,
+        previewFile: undefined
+    };
+    // configure callback hooks so we update on save etc.
+    let onSave = configureHooks(previewer, context);
+
+    // disposer
+    panel.onDidDispose(() => {
+        logChannel.appendLine("INFO: panel disposed");
+        onSave.dispose();
+    });
+    logChannel.appendLine("INFO: Preview panel configured.");
+
+    return previewer;
+
 }
 
 export { Previewer, newPreviewer, configureHooks, updatePreview };
